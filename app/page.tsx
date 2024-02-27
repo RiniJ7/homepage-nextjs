830f8a27381ea9ab902017fbcd4bad9f78146711
rinij <rinimani7@gmail.com>
2024-02-27 15:42:35 -0700

    Hero, Guide, Card, Features, GetApp components created, rendered in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Card from "@/components/Card";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
 
 
 export default function Home() {
   return (
+<>
+<Hero />
+<Guide/>
+<Card/>
+<Features/>
+<GetApp/>
 
-  <h1 className="font-bold text-teal-500 text-2xl"> Hello World !!</h1>
+</>
   )
 }
