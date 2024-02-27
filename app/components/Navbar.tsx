29a43f483a536c83f18dcbe88c1c6d08ec1f9c6f
rinij <rinimani7@gmail.com>
2024-02-27 00:46:52 -0700

    new items added to nav bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link"
+import Image from "next/image"
 
 const Navbar = () => {
   return (
-    
-    <div>Navbar</div>
-  )
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href='/'>
+            Logo
+            {/* <Image src="" alt="logo" width={74} height={29} /> */}
+            <ul className="hidden h-full gap-12 lg:flex">Home</ul>
+            <ul className="hidden h-full gap-12 lg:flex">How we work</ul>
+            <ul className="hidden h-full gap-12 lg:flex">Use our AI matching tools</ul>
+            <ul className="hidden h-full gap-12 lg:flex">Login/SignUp</ul>
+        </Link>
+        </nav>
+ 
+ 
+    )
 }
 
 export default Navbar
