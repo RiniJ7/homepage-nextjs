8d55f09060bcb105abd6e3c25c03da261cc6fd7f
rinij <rinimani7@gmail.com>
2024-02-27 22:32:09 -0700

    image assets added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,9 +32,9 @@ module.exports = {
       backgroundImage: {
         'bg-img-1': "url('/img-1.png')",
         'bg-img-2': "url('/img-2.png')",
-        'feature-bg': "url('/feature-bg.png')",
-        pattern: "url('/pattern.png')",
-        'pattern-2': "url('/pattern-bg.png')",
+        'feature-bg': "url('/feature-bg.jpg')",
+        pattern: "url('/pattern.jpg')",
+        'pattern-2': "url('/pattern-bg.jpeg')",
       },
       screens: {
         xs: '400px',
