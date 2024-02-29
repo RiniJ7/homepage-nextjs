30f685b6a40ddf15002b4db276d341edd44aa19c
rinij <rinimani7@gmail.com>
2024-02-29 14:33:09 -0700

    commented Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,8 @@
+// Import components and constants
 import Image from "next/image"
 import { PEOPLE_URL } from "@/constants"
+
+//define the types of card props 
 interface CardProps {
   backgroundImage : string,
   title: string,
@@ -7,9 +10,11 @@ interface CardProps {
   peopleJoined: string,
 }
 
+//define cards component to render individual card items
 const Cards = ( { backgroundImage, title, subTitle, peopleJoined }: CardProps) => {
 
   return (
+    // card container with dynamic background image
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
 <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
   <div className="flexCenter gap-4">
@@ -21,13 +26,16 @@ width={28}
 height={28}
 />
     </div>
+    {/* title and subtitle */}
     <div>
     <h4 className="flex flex-col gap-1">{title}</h4>
     <p className="regular-14 text-white">{subTitle}</p>
     </div>
   </div >
+  {/* displaying people's images and count */}
 <div className="flexCenter gap-6">
 <span className="flex -space-x-4 overflow-hidden">
+{/* Mapping through PEOPLE_URL array to render people's images */}
 { PEOPLE_URL.map((url) => (
   <Image
   className="inline-block h-10 w-10 rounded-full" 
@@ -39,6 +47,7 @@ height={28}
   />
 ))}
 </span>
+{/* displaying number of people joined */}
 <p className="bold-16 md:bold-20 text-white"> {peopleJoined} </p>
 </div>
 </div>
@@ -51,10 +60,12 @@ height={28}
 
 const Card = () => {
   return (
+    //section container for cards and overlay texts
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-2 xl:mb-20">
+      {/* horizontal scrollable container for cards */}
       <div className="hide-scrollbar flex h-[340px] w-full items-start
       justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-    
+    {/* rendering individual cards */}
 <Cards 
 backgroundImage="bg-bg-img-1"
 title="Job Search"
@@ -68,6 +79,7 @@ subTitle="Find Rentals"
 peopleJoined="75+ Joined"
 />
 </div>
+{/* overlay text and image */}
 <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
 <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
 <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
